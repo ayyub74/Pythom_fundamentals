c9b5e67ef2be522517ee8dbb03dac252a3131cbd
ayyub74 <47635289+ayyub74@users.noreply.github.com>
2020-07-08 22:14:52 -0400

    Add files via upload

---
+++ Python_fundamentals_Day2_B22.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is used to comment the line (use# for commenting lines)
+
+
+# In[2]:
+
+
+#understanding the veriables in python
+
+
+# In[5]:
+
+
+x = 5 # x is holding an value af 5
+y = 3 # y is holding an value of 3
+
+
+# In[6]:
+
+
+x+y
+
+
+# In[7]:
+
+
+# Rule 1 no spave in name
+First_name = 'Ayyub'
+print (First_name)
+
+
+# In[9]:
+
+
+# Rule 1 no spave in name
+name1 = 'Ayb'
+print (name1)
+
+
+# In[11]:
+
+
+#Rule#3 you should not use any special char in variable
+
+name = 'Ayyub'
+print (name)
+
+
+# In[ ]:
+
+
+
+
