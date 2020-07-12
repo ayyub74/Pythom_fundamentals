9b631f1a911621612d9554f0d881cbfb6e6e932c
ayyub74 <47635289+ayyub74@users.noreply.github.com>
2020-07-12 19:45:20 -0400

    Add files via upload

---
+++ Python_fundamentals_Day2_B22 (2).py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is used to comment the line (use# for commenting lines)
+
+
+# In[2]:
+
+
+#understanding the veriables in python
+
+
+# In[5]:
+
+
+x = 5 # x is holding an value af 5
+y = 3 # y is holding an value of 3
+
+
+# In[6]:
+
+
+x+y
+
+
+# In[7]:
+
+
+# Rule 1 no spave in name
+First_name = 'Ayyub'
+print (First_name)
+
+
+# In[9]:
+
+
+# Rule 1 no spave in name
+name1 = 'Ayb'
+print (name1)
+
+
+# In[11]:
+
+
+#Rule#3 you should not use any special char in variable
+
+name = 'Ayyub'
+print (name)
+
+
+# In[18]:
+
+
+# introduction to datatypes in python
+#strings
+#integers
+#list
+#tuple
+#dictionary
+
+
+# In[ ]:
+
+
+# classification of data type:
+
+
+# In[ ]:
+
+
+# datatype ar classified into diff datatype
+
+
+# In[20]:
+
+
+#1 mutable datatype # Declaration : we can able to modify alter
+#2 immutable datatype
+# defination
+# can not able to modify, alter
+
+
+# In[21]:
+
+
+# Python is a case sensative language
+
+
+# In[25]:
+
+
+# introduce to string datatype
+# A string is nothing but a series of charachers enclosed in a code
+# A string is classified as inmutable data type
+# define string---->'ayyub',"ayyub","""Ayyub"""
+
+
+# 
+
+# In[26]:
+
+
+name = 'ayyub'
+print (name)
+
+
+# In[ ]:
+
+
+# buletin method of string
+
+
+# In[28]:
+
+
+type(name) # to verify and validate teh datatype
+
+
+# In[30]:
+
+
+name2 = "ayyub"
+type (name2)
+
+
+# In[31]:
+
+
+name3 = """ayyub"""
+type(name3)
+
+
+# In[35]:
+
+
+full_name = 'ayyub'
+print (full_name)
+
+
+# In[36]:
+
+
+#enhencment of code
+
+
+# In[34]:
+
+
+print(full_name.title())
+
+
+# In[37]:
+
+
+print(full_name.upper()) # upper case
+
+
+# In[42]:
+
+
+print(full_name.lower())  # lower case
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
