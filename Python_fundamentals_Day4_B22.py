ea60bcb58d405b42498321496e763534e80bb240
ayyub74 <47635289+ayyub74@users.noreply.github.com>
2020-07-12 22:11:53 -0400

    Add files via upload

---
+++ Python_fundamentals_Day4_B22.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# introduction to list datatypes
+
+defination: A list is an ordered collection of item
+A list is classified as an mutable datatype (flexible)
+A list is defined by using the square brackets[]
+# In[25]:
+
+
+students= ['ayb', 'ayub', 'Anees', 'sayyed', 'hamza', 'Kausar']
+
+
+# In[26]:
+
+
+type(students)
+
+
+# In[27]:
+
+
+#introduction to indexing: indexing will start from "0"
+    #1,2,3,4.......
+
+
+# In[28]:
+
+
+#how to access or retrive the elements fromt he list
+# i want to access hamza name on output
+
+
+# In[35]:
+
+
+students= ['ayb', 'ayub', 'anees', 'sayyed', 'hamza', 'Kausar']
+print (students[4])
+
+
+# In[36]:
+
+
+print (students[2])
+
+
+# In[39]:
+
+
+print(students[2].title())
+
+
+# In[40]:
+
+
+print(students[3].upper())
+
+
+# In[ ]:
+
+
+# reassingnment, adding, and removing elements by list---!
+
+
+# In[50]:
+
+
+students[0] = 'Ayyubii'
+
+
+# In[51]:
+
+
+# req: what to replace ayb to Ayyubi
+
+
+# In[52]:
+
+
+print(students)
+
+
+# In[53]:
+
+
+students[1] = 'Ayyubsahab'
+
+
+# In[54]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
