81a1241d8bbe39eaca7152cf5fe5e8c68522dc94
ayyub74 <47635289+ayyub74@users.noreply.github.com>
2020-07-14 22:05:05 -0400

    Add files via upload

---
+++ Python_fundamentals_Day6_B22.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Organisation the data with list
+
+students = ['ayyub', 'halima', 'turgut', 'bamsi', 'sayyed', 'hamza', 'z']
+# In[2]:
+
+
+# arrange list by alphabetical order A-Z
+
+
+# In[3]:
+
+
+# two position away
+# temp way
+#permanant way
+
+
+# In[4]:
+
+
+# implemetation of temp using (sorted)
+
+
+# In[5]:
+
+
+students = ['ayyub', 'halima', 'turgut', 'bamsi', 'sayyed', 'hamza']
+
+print (students)
+
+
+# In[6]:
+
+
+print(sorted(students))
+
+
+# In[7]:
+
+
+# permanat way
+
+
+# In[8]:
+
+
+students.sort()
+
+
+# In[9]:
+
+
+print(students)
+
+
+# In[10]:
+
+
+#req: to know thecount of elements are present int he list 
+
+
+# In[11]:
+
+
+len (students)
+
+
+# In[12]:
+
+
+# reverse list in order z-A
+
+
+# In[13]:
+
+
+students.reverse()
+print(students)
+
+
+# In[14]:
+
+
+type(students)
+
+
+# In[15]:
+
+
+students.append('kausar') # will add kausar in list using .append
+
+
+# In[16]:
+
+
+print(students)
+
+
+# In[17]:
+
+
+# general syntax of slicing
+
+
+# In[18]:
+
+
+# varaibale[startpoint:endpoint:stepcount] # last value is always exclasive
+
+
+# In[19]:
+
+
+# two name in same group 
+print(students[0:2])
+
+
+# In[26]:
+
+
+print(students[2:4])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
